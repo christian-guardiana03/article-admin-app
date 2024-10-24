66122f79a4f501388a85f028cdec6b29f23ff878
Christian Rey Guardiana <christian.guardiana@etaily.com>
2024-10-24 11:35:39 +0800

    Removed the register account option on the login page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,9 +39,9 @@
                                 </button>
                             </form>
                             <hr>
-                            <div class="text-center">
+                            {{--<div class="text-center">
                                 <a class="small" href="{{ route('register') }}">Create an Account!</a>
-                            </div>
+                            </div> --}}
                         </div>
                     </div>
                 </div>
